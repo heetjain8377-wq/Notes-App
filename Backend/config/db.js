056c3185bb108f2c65ef55642005d6dd7f677470
heet jain <heetjain8377@gmail.com>
2026-07-05 23:17:22 +0530

    MongoDB Connection Fixing

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
 function connectDB(){
-    mongoose.connect(process.env.MONGO_URI,
-        console.log("Connected To MongoDB")
-    );
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    }catch(error){
+        console.log("MongoDB connection failed");
+        console.log(error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
