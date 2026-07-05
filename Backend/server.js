5bd7a5644707c2d230fcc4bb991c07a6613a089f
heet jain <heetjain8377@gmail.com>
2026-07-05 22:35:58 +0530

    Initial Notes App Commit

--- Backend/server.js
+++ Backend/server.js
@@ -15,6 +15,8 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/note", noteRoutes);
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
